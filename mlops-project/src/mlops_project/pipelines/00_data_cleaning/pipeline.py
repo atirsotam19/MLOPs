c4471b967c5307dff05f6f98df982467408ef014
afonso1012 <afonsolopes1012@gmail.com>
2025-06-17 17:34:27 +0100

    data cleaning continuation

--- mlops-project/src/mlops_project/pipelines/00_data_cleaning/pipeline.py
+++ mlops-project/src/mlops_project/pipelines/00_data_cleaning/pipeline.py
@@ -1,13 +1,13 @@
-from kedro.pipeline import Pipeline, node
-from .nodes import clean_data  # assuming your cleaning function is named clean_data in nodes.py
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import clean_data
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline(
+    return pipeline(
         [
             node(
                 func=clean_data,
-                inputs="raw_data",  # name of the dataset input in catalog
-                outputs="cleaned_data",  # name of the cleaned dataset output
+                inputs="raw_loans",           # Matches the name in catalog.yml
+                outputs="cleaned_loans",      # Matches the name in catalog.yml
                 name="clean_data_node",
             )
         ]
