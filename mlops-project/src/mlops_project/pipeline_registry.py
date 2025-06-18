0da54ee56ead53f2f298269642ee2880646b057f
afonso1012 <afonsolopes1012@gmail.com>
2025-06-18 12:33:59 +0100

    pipeline registry

--- mlops-project/src/mlops_project/pipeline_registry.py
+++ mlops-project/src/mlops_project/pipeline_registry.py
@@ -20,49 +20,75 @@
 from typing import Dict
 from kedro.pipeline import Pipeline, pipeline
 
-from bank_full_project.pipelines import (
-    ingestion as data_ingestion,
-    data_unit_tests as data_tests,
-    preprocessing_train as preprocess_train,
-    split_train_pipeline as split_train,
-    model_selection as model_selection_pipeline,
-    model_train as model_train_pipeline,
-    feature_selection as feature_selection_pipeline,
-    split_data,
-    preprocessing_batch,
-    model_predict
+# CLASS EXAMPLE
 
+#from bank_full_project.pipelines import (
+#    ingestion as data_ingestion,
+#    data_unit_tests as data_tests,
+#    preprocessing_train as preprocess_train,
+#    split_train_pipeline as split_train,
+#    model_selection as model_selection_pipeline,
+#    model_train as model_train_pipeline,
+#    feature_selection as feature_selection_pipeline,
+#    split_data,
+#    preprocessing_batch,
+#    model_predict
+#)
+
+#def register_pipelines() -> Dict[str, Pipeline]:
+#    """Register the project's pipelines.
+
+#    Returns:
+#        A mapping from a pipeline name to a ``Pipeline`` object.
+#    """
+#    ingestion_pipeline = data_ingestion.create_pipeline()
+#    data_unit_tests_pipeline = data_tests.create_pipeline()
+#    split_data_pipeline = split_data.create_pipeline()
+#    preprocess_train_pipeline = preprocess_train.create_pipeline()
+#    split_train_pipeline = split_train.create_pipeline()
+#    model_train = model_train_pipeline.create_pipeline()
+#    model_selection = model_selection_pipeline.create_pipeline()
+#    feature_selection = feature_selection_pipeline.create_pipeline()
+#    preprocess_batch_pipeline = preprocessing_batch.create_pipeline()
+#    model_predict_pipeline = model_predict.create_pipeline()
+
+#    return {
+#        "ingestion": ingestion_pipeline,
+#        "data_unit_tests": data_unit_tests_pipeline,
+#        "split_data": split_data_pipeline,
+#        "preprocess_train": preprocess_train_pipeline,
+#        "split_train": split_train_pipeline,
+#        "model_selection": model_selection,
+#        "model_train": model_train,
+#        "feature_selection":feature_selection,
+#        "production_full_train_process" : preprocess_train_pipeline + split_train_pipeline + model_train,
+#        "preprocess_batch": preprocess_batch_pipeline,
+#        "inference" : model_predict_pipeline,
+#        "production_full_prediction_process" : preprocess_batch_pipeline + model_predict_pipeline
+#    }
+
+# OUR PROJECT
+
+from pipelines import (
+    data_cleaning_00 as data_cleaning_pipeline,
+#    data_unit_tests_01 as data_tests,
+#    data_ingestion_02 as data_ingestion,
+#    data_split_03 as split_data,
+#    preprocessing_train_04 as preprocess_train,
+#    preprocessing_batch_05 as preprocessing_batch,
+#    split_train_pipeline_06 as split_train,
 )
 
 def register_pipelines() -> Dict[str, Pipeline]:
-    """Register the project's pipelines.
-
-    Returns:
-        A mapping from a pipeline name to a ``Pipeline`` object.
-    """
-    ingestion_pipeline = data_ingestion.create_pipeline()
-    data_unit_tests_pipeline = data_tests.create_pipeline()
-    split_data_pipeline = split_data.create_pipeline()
-    preprocess_train_pipeline = preprocess_train.create_pipeline()
-    split_train_pipeline = split_train.create_pipeline()
-    model_train = model_train_pipeline.create_pipeline()
-    model_selection = model_selection_pipeline.create_pipeline()
-    feature_selection = feature_selection_pipeline.create_pipeline()
-    preprocess_batch_pipeline = preprocessing_batch.create_pipeline()
-    model_predict_pipeline = model_predict.create_pipeline()
-
     return {
-        "ingestion": ingestion_pipeline,
-        "data_unit_tests": data_unit_tests_pipeline,
-        "split_data": split_data_pipeline,
-        "preprocess_train": preprocess_train_pipeline,
-        "split_train": split_train_pipeline,
-        "model_selection": model_selection,
-        "model_train": model_train,
-        "feature_selection":feature_selection,
-        "production_full_train_process" : preprocess_train_pipeline + split_train_pipeline + model_train,
-        "preprocess_batch": preprocess_batch_pipeline,
-        "inference" : model_predict_pipeline,
-        "production_full_prediction_process" : preprocess_batch_pipeline + model_predict_pipeline,
-  
+        "data_cleaning": data_cleaning_pipeline.create_pipeline(),
+        #"data_unit_tests": data_tests.create_pipeline(),
+        #"data_ingestion": data_ingestion.create_pipeline(),
+        #"data_split": split_data.create_pipeline(),
+        #"preprocess_train": preprocess_train.create_pipeline(),
+        #"split_train": split_train.create_pipeline(),
+        #"preprocess_batch": preprocessing_batch.create_pipeline(),
+
+        # Optional combo pipelines
+        #"full_training_pipeline": preprocess_train.create_pipeline() + split_train.create_pipeline(),
     }
